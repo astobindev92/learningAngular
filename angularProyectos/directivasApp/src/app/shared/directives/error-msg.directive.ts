bb42ebb05a87693ba0f46d7b0b41822e5eb3f4e5
jonathan cerquera <jonh.cerque1192@gmail.com>
2021-04-07 06:23:24 -0500

    Todavia en el proyecto de directivasAPP

--- angularProyectos/directivasApp/src/app/shared/directives/error-msg.directive.ts
+++ angularProyectos/directivasApp/src/app/shared/directives/error-msg.directive.ts
@@ -1,27 +1,35 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[errorMsg]'
 })
-export class ErrorMsgDirective implements OnInit {
+export class ErrorMsgDirective implements OnInit,OnChanges {
 
 htmlElement:ElementRef<HTMLElement>;
 @Input() color:string='red';
+@Input() mensaje:string='Este campo es necesario';
 
   constructor( private el:ElementRef<HTMLElement>) {
-    console.log('constructor directive');
-    console.log(el);
 
    this.htmlElement= el;
 
   }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+
+  }
   ngOnInit(): void {
-    console.log('NgOnit Directiva');
     this.setColor();
+    this.setMensaje();
   }
 
   setColor():void{
     this.htmlElement.nativeElement.style.color=this.color;
+  }
+
+
+  setMensaje():void{
+    this.htmlElement.nativeElement.innerText=this.mensaje;
   }
 
 }
